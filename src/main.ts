8cb75a3c6faab105be781d23bdb2a0f275bf7087
Craig Wilson <craiggwilson@gmail.com>
2016-02-22 09:37:22 -0600

    working on formatOnSave.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { SongToolsDocumentFormattingEditProvider, Formatter } from './format';
 export function activate(context: vscode.ExtensionContext) {
     
     context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(CHORDSOVERLYRICS_MODE, new SongToolsDocumentFormattingEditProvider()));
+    startBuildOnSaveWatcher(context.subscriptions);
     
     vscode.commands.registerCommand('songtools.formatChordsOverLyrics', () => {
 		vscode.window.showInformationMessage('Formatting as ChordsOverLyrics!');
@@ -29,4 +30,37 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function startBuildOnSaveWatcher(subscriptions: vscode.Disposable[]) {
+
+	// TODO: This is really ugly.  I'm not sure we can do better until
+	// Code supports a pre-save event where we can do the formatting before
+	// the file is written to disk.	
+	let ignoreNextSave = new WeakSet<vscode.TextDocument>();
+
+	vscode.workspace.onDidSaveTextDocument(document => {
+		if (document.languageId !== 'chordsOverLyrics' || ignoreNextSave.has(document)) {
+			return;
+		}
+		let config = vscode.workspace.getConfiguration('songtools');
+		let textEditor = vscode.window.activeTextEditor;
+		let formatPromise: PromiseLike<void> = Promise.resolve();
+		if (config['formatOnSave'] && textEditor.document === document) {
+			let formatter = new Formatter(document.languageId);
+			formatPromise = formatter.formatDocument(document).then(edits => {
+				return textEditor.edit(editBuilder => {
+					edits.forEach(edit => editBuilder.replace(edit.range, edit.newText));
+				});
+			}).then(applied => {
+				ignoreNextSave.add(document);
+				return document.save();
+			}).then(() => {
+				ignoreNextSave.delete(document);
+			}, () => {
+				// Catch any errors and ignore so that we still trigger 
+				// the file save.
+			});
+		}
+	}, null, subscriptions);
 }
